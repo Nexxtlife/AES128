5d95e41922ba0d904e886fb6f05c8fb9c7d3e39c
Pointless1319 <david.matuszak@protonmail.com>
2021-02-10 10:32:23 +0100

    changing display to double segment

--- divider.vhd
+++ divider.vhd
@@ -8,10 +8,25 @@ port(
 
 	in_data : in std_logic_vector(127 downto 0);
 
-	out_data_1 : out std_logic_vector(31 downto 0);
-	out_data_2 : out std_logic_vector(31 downto 0);
-	out_data_3 : out std_logic_vector(31 downto 0);
-	out_data_4 : out std_logic_vector(31 downto 0)
+	out_data_1 : out std_logic_vector(7 downto 0);
+	out_data_2 : out std_logic_vector(7 downto 0);
+	out_data_3 : out std_logic_vector(7 downto 0);
+	out_data_4 : out std_logic_vector(7 downto 0);
+	
+	out_data_11 : out std_logic_vector(7 downto 0);
+	out_data_12 : out std_logic_vector(7 downto 0)
+	--out_data_13 : out std_logic_vector(7 downto 0);
+	--out_data_14 : out std_logic_vector(7 downto 0);
+	
+	--out_data_21 : out std_logic_vector(7 downto 0);
+	--out_data_22 : out std_logic_vector(7 downto 0);
+	--out_data_23 : out std_logic_vector(7 downto 0);
+	--out_data_24 : out std_logic_vector(7 downto 0);
+	
+	--out_data_31 : out std_logic_vector(7 downto 0);
+	--out_data_32 : out std_logic_vector(7 downto 0);
+	--out_data_33 : out std_logic_vector(7 downto 0);
+	--out_data_34 : out std_logic_vector(7 downto 0)
 );
 end entity;
 
@@ -31,10 +46,13 @@ begin
 			out_data_3 <= (others=>'0');
 			out_data_4 <= (others=>'0');
 		else
-			out_data_1 <= in_data(127 downto 96);
-			out_data_2 <= in_data(95 downto 64);
-			out_data_3 <= in_data(63 downto 32);
-			out_data_4 <= in_data(31 downto 0);
+			out_data_1 <= in_data(7 downto 0);
+			out_data_2 <= in_data(15 downto 8);
+			out_data_3 <= in_data(23 downto 16);
+			out_data_4 <= in_data(31 downto 24);
+			out_data_11 <= in_data(39 downto 32);
+			out_data_12 <= in_data(47 downto 40);
+			
 			
 		end if;
 	end if;
--- main.vhd
+++ main.vhd
@@ -4,32 +4,13 @@ use IEEE.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
 
-entity Blinking_led is
+entity main is
 
 port(
 
 rst_n : in std_logic;
 load : in std_logic;
-mode : in std_logic;
 clk : in std_logic;
-led_0 : out std_logic;
-led_1 : out std_logic;
-led_2 : out std_logic;
-led_3 : out std_logic;
-led_4 : out std_logic;
-led_5 : out std_logic;
-led_6 : out std_logic;
-led_7 : out std_logic;
-
-
-bit_0 : in std_logic;
-bit_1 : in std_logic;
-bit_2 : in std_logic;
-bit_3 : in std_logic;
-bit_4 : in std_logic;
-bit_5 : in std_logic;
-bit_6 : in std_logic;
-bit_7 : in std_logic;
 
 hex_0_sig : out std_logic;
 hex_1_sig : out std_logic;
@@ -40,38 +21,111 @@ hex_5_sig : out std_logic;
 hex_6_sig : out std_logic;
 hex_7_sig : out std_logic;
 
-hex_8_sig : out std_logic;
-hex_9_sig : out std_logic;
 hex_10_sig : out std_logic;
 hex_11_sig : out std_logic;
 hex_12_sig : out std_logic;
 hex_13_sig : out std_logic;
 hex_14_sig : out std_logic;
-hex_15_sig : out std_logic
+hex_15_sig : out std_logic;
+hex_16_sig : out std_logic;
+hex_17_sig : out std_logic;
+
+hex_20_sig : out std_logic;
+hex_21_sig : out std_logic;
+hex_22_sig : out std_logic;
+hex_23_sig : out std_logic;
+hex_24_sig : out std_logic;
+hex_25_sig : out std_logic;
+hex_26_sig : out std_logic;
+hex_27_sig : out std_logic;
+
+hex_30_sig : out std_logic;
+hex_31_sig : out std_logic;
+hex_32_sig : out std_logic;
+hex_33_sig : out std_logic;
+hex_34_sig : out std_logic;
+hex_35_sig : out std_logic;
+hex_36_sig : out std_logic;
+hex_37_sig : out std_logic;
+
+hex_40_sig : out std_logic;
+hex_41_sig : out std_logic;
+hex_42_sig : out std_logic;
+hex_43_sig : out std_logic;
+hex_44_sig : out std_logic;
+hex_45_sig : out std_logic;
+hex_46_sig : out std_logic;
+hex_47_sig : out std_logic;
+
+hex_50_sig : out std_logic;
+hex_51_sig : out std_logic;
+hex_52_sig : out std_logic;
+hex_53_sig : out std_logic;
+hex_54_sig : out std_logic;
+hex_55_sig : out std_logic;
+hex_56_sig : out std_logic;
+hex_57_sig : out std_logic
+
+
 );
 end entity;
 
-architecture behave of Blinking_led is
+architecture behave of main is
  
  signal data : std_logic_vector(127 downto 0);
- signal hex_display_0 : std_logic_vector(7 downto 0);
- signal hex_display_1 : std_logic_vector(7 downto 0);
- signal hex_display_2 : std_logic_vector(7 downto 0);
- signal hex_display_3 : std_logic_vector(7 downto 0);
-	
+ signal hex_data_0 : std_logic_vector(3 downto 0);
+ signal hex_data_1 : std_logic_vector(3 downto 0);
+ signal hex_data_2 : std_logic_vector(3 downto 0);
+ signal hex_data_3 : std_logic_vector(3 downto 0);
+ signal hex_data_4 : std_logic_vector(3 downto 0);
+ signal hex_data_5 : std_logic_vector(3 downto 0);
+ signal hex_data_6 : std_logic_vector(3 downto 0);
+
+	component byteSub_display_sys is
+		port (
+			bytesub_0_avalon_streaming_sink_data : in  std_logic_vector(127 downto 0) := (others => 'X'); -- data
+			clk_clk                              : in  std_logic                      := 'X';             -- clk
+			reset_reset_n                        : in  std_logic                      := 'X';             -- reset_n
+			divider_0_data_out_1_data            : out std_logic_vector(3 downto 0);                      -- data
+			divider_0_data_out_2_data            : out std_logic_vector(3 downto 0);                      -- data
+			divider_0_data_out_3_data            : out std_logic_vector(3 downto 0);                      -- data
+			divider_0_data_out_4_data            : out std_logic_vector(3 downto 0);                      -- data
+			divider_0_data_out_5_data            : out std_logic_vector(3 downto 0);                      -- data
+			divider_0_data_out_6_data            : out std_logic_vector(3 downto 0)                       -- data
+		);
+	end component byteSub_display_sys; 
  
-	
- 
+	component display
+		port(
+			data_in : in std_logic_vector(3 downto 0);
+			hex_0 : out std_logic;
+			hex_1 : out std_logic;
+			hex_2 : out std_logic;
+			hex_3 : out std_logic;
+			hex_4 : out std_logic;
+			hex_5 : out std_logic;
+			hex_6 : out std_logic;
+			hex_7 : out std_logic
+		);
+	end component;
 
  begin
  
-
-
-
+ u0 : component byteSub_display_sys
+		port map (
+			bytesub_0_avalon_streaming_sink_data => data, -- bytesub_0_avalon_streaming_sink.data
+			clk_clk                              => clk,                              --                             clk.clk
+			reset_reset_n                        => rst_n,                        --                           reset.reset_n
+			divider_0_data_out_1_data            => hex_data_0,            --            divider_0_data_out_1.data
+			divider_0_data_out_2_data            => hex_data_1,            --            divider_0_data_out_2.data
+			divider_0_data_out_3_data            => hex_data_2,            --            divider_0_data_out_3.data
+			divider_0_data_out_4_data            => hex_data_3,            --            divider_0_data_out_4.data
+			divider_0_data_out_5_data            => hex_data_4,            --            divider_0_data_out_5.data
+			divider_0_data_out_6_data            => hex_data_5             --            divider_0_data_out_6.data
+		);
  
- 
- d1 : display port map (
-					data_in => data_in_sig, 
+ d0 : display port map (
+					data_in => hex_data_0, 
 					hex_0 => hex_0_sig,
 					hex_1 => hex_1_sig,
 					hex_2 => hex_2_sig,
@@ -81,48 +135,79 @@ architecture behave of Blinking_led is
 					hex_6 => hex_6_sig,
 					hex_7 => hex_7_sig
 				);
+ d1 : display port map (
+					data_in => hex_data_1, 
+					hex_0 => hex_10_sig,
+					hex_1 => hex_11_sig,
+					hex_2 => hex_12_sig,
+					hex_3 => hex_13_sig,
+					hex_4 => hex_14_sig,
+					hex_5 => hex_15_sig,
+					hex_6 => hex_16_sig,
+					hex_7 => hex_17_sig
+				);
+
  d2 : display port map (
-					data_in => data_in_sig2, 
-					hex_0 => hex_8_sig,
-					hex_1 => hex_9_sig,
-					hex_2 => hex_10_sig,
-					hex_3 => hex_11_sig,
-					hex_4 => hex_12_sig,
-					hex_5 => hex_13_sig,
-					hex_6 => hex_14_sig,
-					hex_7 => hex_15_sig
+					data_in => hex_data_2, 
+					hex_0 => hex_20_sig,
+					hex_1 => hex_21_sig,
+					hex_2 => hex_22_sig,
+					hex_3 => hex_23_sig,
+					hex_4 => hex_24_sig,
+					hex_5 => hex_25_sig,
+					hex_6 => hex_26_sig,
+					hex_7 => hex_27_sig
+				);
+ d3 : display port map (
+					data_in => hex_data_3, 
+					hex_0 => hex_30_sig,
+					hex_1 => hex_31_sig,
+					hex_2 => hex_32_sig,
+					hex_3 => hex_33_sig,
+					hex_4 => hex_34_sig,
+					hex_5 => hex_35_sig,
+					hex_6 => hex_36_sig,
+					hex_7 => hex_37_sig
+				);
+ d4 : display port map (
+					data_in => hex_data_4, 
+					hex_0 => hex_40_sig,
+					hex_1 => hex_41_sig,
+					hex_2 => hex_42_sig,
+					hex_3 => hex_43_sig,
+					hex_4 => hex_44_sig,
+					hex_5 => hex_45_sig,
+					hex_6 => hex_46_sig,
+					hex_7 => hex_47_sig
+				);
+ d5 : display port map (
+					data_in => hex_data_5, 
+					hex_0 => hex_50_sig,
+					hex_1 => hex_51_sig,
+					hex_2 => hex_52_sig,
+					hex_3 => hex_53_sig,
+					hex_4 => hex_54_sig,
+					hex_5 => hex_55_sig,
+					hex_6 => hex_56_sig,
+					hex_7 => hex_57_sig
 				);
  
-led_0 <= number_last(0);
-led_1 <= number_last(1); 
-led_2 <= number_last(2);
-led_3 <= number_last(3);
-led_4 <= number_last(4);
-led_5 <= number_last(5);
-led_6 <= number_last(6);
-led_7 <= number_last(7);	
-data_in_sig <= number_new(3 downto 0);
-data_in_sig2 <= number_new(7 downto 4);
+hex_0_sig <= hex_data_0(0);
+hex_1_sig <= hex_data_0(1);
+hex_2_sig <= hex_data_0(2);
+hex_3_sig <= hex_data_0(3);
+hex_4_sig <= hex_data_0(4);
+hex_5_sig <= hex_data_0(5);
+
 
 	process (clk)
 	begin
 	if rising_edge(clk) then
 		if rst_n = '0' then
 			
-			number_last <= (others => '0');
+			hex_data_5 <= (others => '0');
 		else
-			number_new <= bit_7 & bit_6 & bit_5 & bit_4 & bit_3 & bit_2 & bit_1 & bit_0;
-			
-			if mode = '0'then
 
-				if load = '0' then
-					number_last <= number_new;
-					end if;
-			else
-				if load = '0' then
-					number_last <= number_last xor number_new;
-				end if;
-			end if;
 		end if;
 	end if;
 	
